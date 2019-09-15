0b0c5049d9125305d5e3f0eb4f231a554514c80b
Alper <31041738+4ppe@users.noreply.github.com>
2019-09-15 18:09:45 +0300

    get & post method edit

--- api/app.js
+++ api/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
 
 // Load in the mongoose models
 const{ List, Task} = require('./db/models/index');
+
+// Load middleware
+app.use(bodyParser.json());
+
 
 /* ROUTE HANDLERS*/
 
@@ -17,16 +24,27 @@ app.get('/lists',async (req,res) => {
     // We want to return an array of all the lists in the database
     const lists = await List.find({})
     res.send(lists);
-})
+});
 
 
 /**
  * POST /lists
  * Purpose: Create a list
  */
-app.post('/lists',(req,res) => {
+app.post('/lists',async (req,res) => {
     // We want to create a new list and return the new list document back to the user (which includes the id)
     // The list information (fields) will be passed in via the JSON request body
+    let title = req.body.title;
+
+    let newList = new List({
+        title
+    });
+    
+    newList.save().then((listDoc) => {
+       // The full list document is returned (incl. id)
+    res.send(listDoc); 
+    })
+    
 });
 
 
